25fcc1db4fe41da0405fca313a4825590847da07
Denis Arkhireev <den-uln@mail.ru>
2018-07-27 03:34:14 +0400

    Added Tower script, snapping turret to top of tower

---
+++ TowerDefense/Assets/Scripts/Tower.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[ExecuteInEditMode]
+public class Tower : MonoBehaviour
+{
+    private const float TOWER_HEIGHT = 2.7f;
+    private static readonly Vector3 SnappingPoint = new Vector3(0, TOWER_HEIGHT, 0);
+
+    [SerializeField] private GameObject turret;
+
+    private void Update()
+    {
+        SetTurret();
+    }
+
+    private void SetTurret()
+    {
+        turret.transform.localPosition = SnappingPoint;
+    }
+}

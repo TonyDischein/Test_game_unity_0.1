12189d5c3d2c47148e373abe31d0244f12694ff5
Denis Arkhireev <den-uln@mail.ru>
2018-07-30 15:43:48 +0400

    Added detecting enemy script

--- TowerDefense/Assets/Scripts/Tower.cs
+++ TowerDefense/Assets/Scripts/Tower.cs
@@ -1,9 +1,5 @@
 ﻿using UnityEngine;
 
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
 [ExecuteInEditMode]
 public class Tower : MonoBehaviour
 {
@@ -11,14 +7,62 @@ public class Tower : MonoBehaviour
     private static readonly Vector3 SnappingPoint = new Vector3(0, TOWER_HEIGHT, 0);
 
     [SerializeField] private GameObject turret;
+    [SerializeField] private GameObject pivot;
+    [SerializeField] private GameObject muzzle;
+    [SerializeField] private float enemyDetectionDistance = 10f;
+
+    private Weapoints nearestEnemy;
 
     private void Update()
     {
-        SetTurret();
+        if (Application.isPlaying) {
+            FindNearestEnemy();
+        } else {
+            SetTurret();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.up, enemyDetectionDistance);
     }
 
     private void SetTurret()
     {
         turret.transform.localPosition = SnappingPoint;
+    }
+
+    private void FindNearestEnemy()
+    {
+        if (nearestEnemy == null)
+        {
+            Weapoints[] enemies = FindObjectsOfType<Weapoints>();
+            if (enemies.Length > 0)
+            {
+                foreach (var enemy in enemies)
+                {
+                    if (Vector3.Distance(transform.position, enemy.transform.position) <= enemyDetectionDistance)
+                    {
+                        nearestEnemy = enemy;
+                        Debug.Log("Enemy found");
+                        return;
+                    }
+                }
+                Debug.Log("There is no enemies");
+            }
+        }
+        else
+        {
+            if (Vector3.Distance(transform.position, nearestEnemy.transform.position) > enemyDetectionDistance)
+            {
+                nearestEnemy = null;
+                Debug.Log("Enemy left detection zone");
+            }
+        }
+    }
+
+    private void RotateTurretToNearestEnemy()
+    {
+        
     }
 }
